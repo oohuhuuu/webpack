1950e2b2136f334d8e50eebf0488f7c469f7b204
pphuhuuu <764570930@qq.com>
2016-10-10 12:11:38 +0800

    1
    1

---
+++ src/scripts/page/about.js
@@ -0,0 +1,7 @@
+//引入css
+require("../../styles/lib/reset.css");
+require("../../styles/common/global.css");
+require("../../styles/common/grid.css");
+require("../../styles/common/common.less");
+
+$('.g-bd').append('这是about的js文件');
---
+++ src/scripts/page/index.js
@@ -0,0 +1,20 @@
+//引入css
+require("../../styles/lib/reset.css");
+require("../../styles/common/global.css");
+require("../../styles/common/grid.css");
+require("../../styles/common/common.less");
+require("../../styles/page/index.less");
+
+require("../components/ss.js");
+
+var oP = document.createElement('p');
+oP.className = 'text';
+oP.innerHTML = '这是由js生成的一句话。';
+document.querySelector('.g-bd').appendChild(oP);
+
+//增加事件
+$('.btn').click(function() {
+	require(['../components/dialog/index.js'], function(dialog) {
+		dialog();
+	});
+});
